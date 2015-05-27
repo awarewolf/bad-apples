5677004c71412897462e9b2ad2c864a08219a666
awarewolfyukon <simon.pregent@hec.ca>
2015-05-27 05:33:11 +0000

    11

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  # get 'users/new'
-
-  # get 'users/create'
-
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
